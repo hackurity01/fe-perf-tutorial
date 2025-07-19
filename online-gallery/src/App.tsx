269689794526a8ec9bafff7f99ac4c1167604b56
hackurity01 <hackurity01@gmail.com>
2025-07-20 00:34:33 +0900

    Bundle Analyzer 분석 및 페이지 Lazy load

--- online-gallery/src/App.tsx
+++ online-gallery/src/App.tsx
@@ -1,8 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { CartProvider } from "./contexts/CartContext";
+import { lazy, Suspense } from "react";
 import HomePage from "./pages/HomePage";
-import CartPage from "./pages/CartPage";
+
+const CartPage = lazy(() => import("./pages/CartPage"));
 
 const queryClient = new QueryClient();
 
@@ -11,10 +13,12 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <CartProvider>
         <Router>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/cart" element={<CartPage />} />
-          </Routes>
+          <Suspense fallback={"로딩 중..."}>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/cart" element={<CartPage />} />
+            </Routes>
+          </Suspense>
         </Router>
       </CartProvider>
     </QueryClientProvider>
