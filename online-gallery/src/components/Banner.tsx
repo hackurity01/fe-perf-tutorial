1ed3e4cbe4a8a7d14bc379ebdb2ab12e6a9cabe8
hackurity01 <hackurity01@gmail.com>
2025-07-19 20:01:32 +0900

    LCP 최적화 1: 배너 이미지 직접 로드하기

--- online-gallery/src/components/Banner.tsx
+++ online-gallery/src/components/Banner.tsx
@@ -1,23 +1,9 @@
-import { useQuery } from "@tanstack/react-query";
-
-const fetchBanner = async () => {
-  await new Promise((resolve) => setTimeout(resolve, 500));
-  const res = await fetch("/api/banner.json");
-  if (!res.ok) throw new Error("Network response was not ok");
-  return res.json();
-};
-
 function Banner() {
-  const { data: banner } = useQuery({
-    queryKey: ["banner"],
-    queryFn: fetchBanner,
-  });
+  const banner = "/event-banner.webp";
 
   return (
     <div className="w-full bg-[#1a2a3a] flex justify-center items-center mb-14 h-[500px]">
-      {banner && (
-        <img src={banner?.banner} alt="Event Banner" className="block h-full" />
-      )}
+      <img src={banner} alt="Event Banner" className="block h-full" />
     </div>
   );
 }
